3811386827a2be7d2d25ac0c82394b69c668bbda
Rishi Satsangi <rishi.satsangi@gmail.com>
2022-11-01 14:14:07 -0400

    added slim

--- public/index.php
+++ public/index.php
@@ -1,1 +1,47 @@
-<?php echo "Hiya"; ?>
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors',1);
+define("APPLICATION_PATH", __DIR__ . "/../");
+date_default_timezone_set('America/New_York');
+session_cache_limiter(false);
+session_start();
+
+# Ensure src/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    APPLICATION_PATH ,
+    APPLICATION_PATH . 'library',
+    get_include_path(),
+)));
+
+
+require '../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Slim\Factory\AppFactory;
+
+
+// Create app
+$app = AppFactory::create();
+
+$twig = Twig::create('../views',
+    ['cache' => false]);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+
+# all GET routes
+$app->get('/', function (Request $request, Response $response, $args) {
+
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.html', [
+        'prompt' => 'pizza'
+    ]);
+})->setName('printPrompt');
+
+$app->run();
