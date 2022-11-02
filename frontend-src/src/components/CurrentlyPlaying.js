de64892963bc885a0daf3de6273fdb425027d860
Rishi Satsangi <rishi.satsangi@gmail.com>
2022-11-02 15:20:32 -0400

    fetch currently playing from component

--- frontend-src/src/components/CurrentlyPlaying.js
+++ frontend-src/src/components/CurrentlyPlaying.js
@@ -1,8 +1,37 @@
+import React, { useState, useEffect } from "react";
+
 function CurrentlyPlaying() {
-    return (
-      <div>currently playing</div>
-    );
-  }
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/v1/me/player/currently-playing")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setItem(result);
+            },
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+          )
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                {item.id}
+            </div>
+        );
+    }
+}
   
-  export default CurrentlyPlaying;
+export default CurrentlyPlaying;
   
